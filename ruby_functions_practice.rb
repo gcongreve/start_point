bcd00f13c6c829addb14cffb6ba875732c1880ab
Graeme Congreve <junkmailgmc@gmail.com>
2018-10-16 17:15:52 +0100

    refactoring short month and full month functions

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -31,24 +31,62 @@ def add_string_as_number(string1, string2)
 end
 
 def number_to_full_month_name(number)
-  if (number == 1)
+  case number
+  when  1
     return "January"
-  elsif (number == 3)
+  when  2
+    return "February"
+  when  3
     return "March"
-  elsif (number == 9)
+  when 4
+    return "April"
+  when  5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when  8
+    return "August"
+  when  9
     return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
   end
 end
 
 def number_to_short_month_name(number)
-  if (number == 1)
-    return "Jan"
-  elsif (number == 4)
-    return "Apr"
-  elsif (number == 10)
-    return "Oct"
+    case number
+    when  1
+      return "Jan"
+    when  2
+      return "Feb"
+    when  3
+      return "Mar"
+    when 4
+      return "Apr"
+    when  5
+      return "May"
+    when 6
+      return "Jun"
+    when 7
+      return "Jul"
+    when  8
+      return "Aug"
+    when  9
+      return "Sep"
+    when 10
+      return "Oct"
+    when 11
+      return "Nov"
+    when 12
+      return "Dec"
+    end
   end
-end
 
 def volume_of_cube(length)
   return length * length * length
